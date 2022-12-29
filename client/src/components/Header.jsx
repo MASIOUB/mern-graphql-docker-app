b0ced213d1a495e730e7d7369ac7c31f48c221de
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-12-29 16:31:30 +0100

    set up the frontend

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <nav className='navbar bg-light mb-4 p-0'>
+      <div className='container'>
+        <a className='navbar-brand' href='/'>
+          <div className='d-flex'>
+            <h1>Logo</h1>
+          </div>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
